c4ae3a6e7633db2aba82f39de64cb90068c36e90
Daniel Valenzuela <hetzondaniel1@gmial.com>
2024-05-30 10:34:56 -0600

    Se agrego el font del login

---
+++ ProyectoDB1/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoDB1.Models;
+namespace ProyectoDB1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoDB1/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyectoDB1.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; } 
+        public String Nombre { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
